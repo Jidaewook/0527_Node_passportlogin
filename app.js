c5d2e918dc98aab7a49f46a5894e03db51f8ed58
Jay <psatdoctor@naver.com>
2019-05-27 21:50:43 +0900

    Add views file

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const app = express();
+
+//EJS 사용가능케 하기, EJS 홈페이지에서 사용법 확인
+app.use(expressLayout);
+app.set('view engine', 'ejs');
+
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
