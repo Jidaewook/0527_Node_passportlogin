28771f0f8c4d4536e0ea61d9e768d513c0a7d9b6
Jay <psatdoctor@naver.com>
2019-06-05 20:43:18 +0900

    register function and add message

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcryptjs');
+
+const userModel = require('../models/User');
+
+
 
 //로그인 페이지
 router.get('/login', (req, res) => {
@@ -13,8 +18,77 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('hello');
+    // register에 적는 내용이 터미널에 뜬다. 이것을 파라미터로 보내주면 된다. 
+    // console.log(req.body);
+    // res.send('hello');
+
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+
+    //Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({msg: 'Please fill in all fields'});
+    }
+
+    //Check passwords match
+    if(password !== password2){
+        errors.push({msg: 'Passwords do not match'});
+    }
+
+    //Check passwords length
+    if(password.length < 6){
+        errors.push({msg: 'Password should be at least 6 characters'});
+    
+    }
+
+    if(errors.length > 0){
+        res.render('register', {
+           errors,
+           name,
+           email,
+           password,
+           password2 
+        });
+    } else {
+        userModel.findOne({email: email})
+            .then(user => { 
+                if(user){
+                    errors.push({msg: 'Email already exists'});
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new userModel({
+                        name,
+                        email,
+                        password
+                    });
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+                            newUser.password = hash;
+                            newUser
+                                .save()
+                                .then(user => {
+                                    req.flash(
+                                        'success_msg',
+                                        'You are now registered and can log in'
+                                    );
+                                })
+                                .catch(err => console.log(err));
+                        })
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+
 });
 
 
