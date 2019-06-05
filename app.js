4b2e7a1792a569d17b359e607d21233d36193884
Jay <psatdoctor@naver.com>
2019-06-05 21:19:30 +0900

    add passport

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
+
 
 const app = express();
+
+//Passport config
+require('./config/passport')(passport);
+
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -37,6 +43,10 @@ app.use((req, res, next) => {
     res.locals.error_msg = req.flash('error_msg');
     next();
 });
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
