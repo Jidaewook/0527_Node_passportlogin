d22077be4dbc5b221331f5be3905ee7a08cebb73
Jay <psatdoctor@naver.com>
2019-06-05 21:04:34 +0900

    add bcrypt, hash, flash

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -20,8 +22,21 @@ app.set('view engine', 'ejs');
 // Express body parser
 app.use(express.urlencoded({extended: true}));
 
+app.use(session({
+    secret: 'sectet',
+    resave: true,
+    saveUninitialized: true
+}));
 
+//Connect flash
+app.use(flash());
 
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
